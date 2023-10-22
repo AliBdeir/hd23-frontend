d065b3ad708ac6f55d5f2a8148b5e3c4df3d1f0c
Ali Bdeir <developer.ali.bdeir@gmail.com>
2023-10-22 08:53:11 -0400

    Chapter list styles

--- src/pages/ResultPage/index.tsx
+++ src/pages/ResultPage/index.tsx
@@ -47,7 +47,7 @@ export function ResultPage() {
     queryKey: ["overview", sessionId],
   });
 
-  const [expanded, setExpanded] = useState<number[]>([]);
+  const [expanded, setExpanded] = useState<number[]>([0, 1, 2]);
 
   const handleChange = (index: number) => (_: unknown, isExpanded: boolean) => {
     const clone = [...expanded];
@@ -65,52 +65,61 @@ export function ResultPage() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold py-16">Upload is successful here are the chapters of that book:</h1>
-      {query.data?.data &&
-        query.data.data.map((chapter, i) => {
-          return (
-            <Accordion
-              style={{
-                padding: "10px 10px 50px 10px",
-              }}
-              expanded={expanded.includes(i)}
-              onChange={handleChange(i)}
-              key={i}
-            >
-              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-                <Typography variant="h5" fontWeight="bold">
-                  {chapter.title}
-                </Typography>
-              </AccordionSummary>
+      {/* <h1 className="text-3xl font-bold py-16">Upload is successful here are the chapters of that book:</h1> */}
+      <div className="my-4">
+        <Typography variant="h2" className="text-center">
+          Welcome, Ali
+        </Typography>
+      </div>
+      <div className="flex flex-col gap-4">
+        {query.data?.data &&
+          query.data.data.map((chapter, i) => {
+            return (
+              <Accordion
+                expanded={expanded.includes(i)}
+                onChange={handleChange(i)}
+                key={i}
+                className="p-6"
+                style={{
+                  animationDelay: `${0.1 * (i + 1)}s`,
+                  animation: `popUpEffect ${0.5 * (i + 1)}s forwards`,
+                }}
+              >
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography variant="h5" fontWeight="bold">
+                    {chapter.title}
+                  </Typography>
+                </AccordionSummary>
 
-              <AccordionDetails>
-                <List>
-                  <MyListItem
-                    text="Flash Cards!"
-                    icon={<FlashOnIcon />}
-                    handleClick={() => {
-                      navigate(`/result/${sessionId}/${chapter.id}/flash`);
-                    }}
-                  />
-                  <MyListItem
-                    text="Quiz"
-                    icon={<QuizIcon />}
-                    handleClick={() => {
-                      navigate(`/result/${sessionId}/${chapter.id}/quiz`);
-                    }}
-                  />
-                  <MyListItem
-                    text="Assignment"
-                    icon={<AssignmentIcon />}
-                    handleClick={() => {
-                      navigate(`/result/${sessionId}/${chapter.id}/assignment`);
-                    }}
-                  />
-                </List>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
+                <AccordionDetails>
+                  <List>
+                    <MyListItem
+                      text="Flash Cards!"
+                      icon={<FlashOnIcon />}
+                      handleClick={() => {
+                        navigate(`/result/${sessionId}/${chapter.id}/flash`);
+                      }}
+                    />
+                    <MyListItem
+                      text="Quiz"
+                      icon={<QuizIcon />}
+                      handleClick={() => {
+                        navigate(`/result/${sessionId}/${chapter.id}/quiz`);
+                      }}
+                    />
+                    <MyListItem
+                      text="Assignment"
+                      icon={<AssignmentIcon />}
+                      handleClick={() => {
+                        navigate(`/result/${sessionId}/${chapter.id}/assignment`);
+                      }}
+                    />
+                  </List>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+      </div>
     </div>
   );
 }
