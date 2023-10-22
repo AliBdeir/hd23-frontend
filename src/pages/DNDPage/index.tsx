79e9a071a0d1a15293f7204f5cbfc4f79bbb9623
khanabdu25 <khanabdu@umich.edu>
2023-10-22 09:08:17 -0400

    circ prog for upload file

--- src/pages/DNDPage/index.tsx
+++ src/pages/DNDPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { useNavigate } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
@@ -8,34 +8,42 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import logo from '../../assets/images/coursify_logo.png';
 import axios from 'axios';
+import { CircularProgress } from '@mui/material';
 
 function MyDropzone() {
     const navigate = useNavigate();
-    
+    const [uploading, setUploading] = useState(false)
+
     const onDrop = useCallback(async (acceptedFiles) => {
+        setUploading(true)
+
         const file = acceptedFiles[0];
 
         const formData = new FormData();
         formData.append('file', file);
 
-        navigate("/result")
+        // navigate("/result")
 
-        // try {
-        //     const response = await axios.post('your_server_base_url/api/pdf/upload', formData, {
-        //         headers: {
-        //             'Content-Type': 'multipart/form-data',
-        //         },
-        //     });
-        //     console.log('File uploaded successfully:', response.data);
-        // } catch (error) {
-        //     console.error('Error uploading file:', error);
-        // }
+        try {
+            const response = await axios.post<string>('pdf/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            console.log('File uploaded successfully:', response.data);
+            navigate(`/result/${response.data}`)
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        } finally {
+            setUploading(false)
+        }
     }, []);
 
     const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
-    return (
-        <Paper
+    return <div className="flex justify-center">
+        {uploading && <CircularProgress />}
+        {!uploading && <Paper
             elevation={3}
             {...getRootProps()}
             className="p-6 border-2 border-dashed border-gray-300 bg-gray-100 rounded-md text-center cursor-pointer"
@@ -44,8 +52,8 @@ function MyDropzone() {
             <input {...getInputProps()} />
             <Typography variant="h6" color="textSecondary">
                 Drag n drop some files here, or click to select files</Typography>
-        </Paper>
-    )
+        </Paper>}
+    </div>
 }
 
 
@@ -58,7 +66,7 @@ export default function DNDPage() {
                 <div className="flex flex-col items-center justify-center h-64 max-w-md mx-auto my-12">
                     <img src={logo} alt="Logo" className="max-w-xs max-h-xs w-full h-auto" /> {/* Tailwind CSS classes for size and other styles */}
                 </div>
-                <MyDropzone />
+                 <MyDropzone />
                 {/* Other components can go here */}
             </div>
         </div>
